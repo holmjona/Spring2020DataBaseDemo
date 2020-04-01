b66b574f479e999e1929f343bbc38bc12bb2a279
Jon Holmes <holmjona@isu.edu>
2020-03-31 19:15:23 -0600

    Add project files.

---
+++ DataBaseDemo/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBaseDemo {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e) {
+            // Get data from database
+            SqlConnection conn = null;
+
+            try {
+                // Create Connection
+                conn = new SqlConnection();
+                //conn.ConnectionString = "Server=localhost;Database=SuperHeroes;User Id=holmjona;Password=password;";
+                conn.ConnectionString = "Data Source=localhost;Initial Catalog=SuperHeroes;Integrated Security=True";
+
+                // Open Connection
+                conn.Open();
+                //tbOutput.Text = "I connected";
+
+                // Create Command
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = conn;
+                comm.CommandType = System.Data.CommandType.Text;
+                comm.CommandText = "SELECT * FROM SuperHeroes";
+
+                // Execute Command
+                SqlDataReader dr = comm.ExecuteReader();
+
+                // Read Results
+
+                // FIll from Database
+                while (dr.Read()) {
+                    SuperHero sup = new SuperHero();
+                    sup.ID = (int)dr["SuperHeroID"];
+                    sup.FirstName = (string)dr["FirstName"];
+                    sup.LastName = (string)dr["LastName"];
+                    sup.Height = (decimal)dr["HeightInInches"];
+                    lbSupers.Items.Add(sup);
+                }
+
+                
+
+            } catch (Exception ex) {
+                tbOutput.Text = "Error: " + ex.Message;
+            } finally {
+                // Close Connection
+                if(conn != null) conn.Close();
+            }
+        }
+    }
+}
---
+++ DataBaseDemo/SuperHero.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseDemo {
+    class SuperHero {
+        #region Private Variables
+        private int _ID;
+		private string _FirstName;
+		private string _LastName;
+		private DateTime _DateOfBirth;
+		private decimal _Height;
+        #endregion
+
+        #region Public Properties
+        public int ID {
+			get { return _ID; }
+			set { _ID = value; }
+		}
+
+
+		public string FirstName {
+			get { return _FirstName; }
+			set { _FirstName = value; }
+		}
+
+
+		public string LastName {
+			get { return _LastName; }
+			set { _LastName = value; }
+		}
+
+
+		public DateTime DateOfBirth {
+			get { return _DateOfBirth; }
+			set { _DateOfBirth = value; }
+		}
+
+
+		public decimal Height {
+			get { return _Height; }
+			set { _Height = value; }
+		}
+
+		#endregion
+
+		public override string ToString() {
+			return FirstName + " " + LastName;
+		}
+
+	}
+}
